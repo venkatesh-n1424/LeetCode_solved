0f9f1ec71af075f9bbb2a0b74a3a1c4ee51068be
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-05 12:34:48 +0530

    Time: 55 ms (16.95%) | Memory: 75.6 MB (32.41%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,23 +10,67 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        if(!head || !head->next) return head;
-        priority_queue<int,vector<int>,greater<int>> pq;
-        ListNode* cur=head;
-        while(cur){
-            pq.push(cur->val);
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dnode = new ListNode(-1);
+        ListNode* l1=list1, *l2=list2, *cur=dnode;
+        while(l1 && l2){
+            if(l1->val<=l2->val){
+                cur->next=l1;
+                cur=cur->next;
+                l1=l1->next;
+            }
+            else{
+                cur->next=l2;
+                cur=cur->next;
+                l2=l2->next;
+            }
+        }
+        while(l1){
+            cur->next=l1;
             cur=cur->next;
+            l1=l1->next;
         }
-        ListNode* new_head=new ListNode(pq.top());
-        pq.pop();
-        cur=new_head;
-        while(!pq.empty()){
-            ListNode* node=new ListNode(pq.top());
-            cur->next=node;
-            cur=node;
-            pq.pop();
+        while(l2){
+            cur->next=l2;
+            cur=cur->next;
+            l2=l2->next;
         }
-        return new_head;
+        return dnode->next;
+    }
+    ListNode* findMiddle(ListNode* head){
+        ListNode* s=head;
+        ListNode* f=head->next;
+        while(f && f->next){
+            s=s->next;
+            f=f->next->next;
+        }
+        return s;
+    }
+    ListNode* sortList(ListNode* head) {
+        // if(!head || !head->next) return head;
+        // priority_queue<int,vector<int>,greater<int>> pq;
+        // ListNode* cur=head;
+        // while(cur){
+        //     pq.push(cur->val);
+        //     cur=cur->next;
+        // }
+        // ListNode* new_head=new ListNode(pq.top());
+        // pq.pop();
+        // cur=new_head;
+        // while(!pq.empty()){
+        //     ListNode* node=new ListNode(pq.top());
+        //     cur->next=node;
+        //     cur=node;
+        //     pq.pop();
+        // }
+        // return new_head;
+        //optimal
+        if(!head || !head->next) return head;
+        ListNode* middle=findMiddle(head);
+        ListNode* lefthead=head,*righthead=middle->next;
+        middle->next=nullptr;
+        lefthead=sortList(lefthead);
+        righthead=sortList(righthead);
+        return mergeTwoLists(lefthead,righthead);
     }
 };
