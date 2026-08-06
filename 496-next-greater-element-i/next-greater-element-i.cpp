a3bac25172d2a6558dd784e1dc6ca398fa4e555f
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-06 22:38:17 +0530

    Time: 2 ms (47.08%) | Memory: 12.7 MB (76.62%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,21 +3,50 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         //brute
         int n1=nums1.size(),n2=nums2.size();
-        vector<int> ans;
-        for(int i=0;i<n1;i++){
-            for(int j=0;j<n2;j++){
-                if(nums1[i]==nums2[j]){
-                    bool have=false;
-                    for(int k=j+1;k<n2;k++){
-                        if(nums2[j]<nums2[k]){
-                            have=true;
-                            ans.emplace_back(nums2[k]);
-                            break;
-                        }
-                    }
-                    if(!have) ans.emplace_back(-1);
+        vector<int> ans(n1,-1);
+        unordered_map<int,int> d;
+        // for(int i=0;i<n1;i++){
+        //     for(int j=0;j<n2;j++){
+        //         if(nums1[i]==nums2[j]){
+        //             bool have=false;
+        //             for(int k=j+1;k<n2;k++){
+        //                 if(nums2[j]<nums2[k]){
+        //                     have=true;
+        //                     ans.emplace_back(nums2[k]);
+        //                     break;
+        //                 }
+        //             }
+        //             if(!have) ans.emplace_back(-1);
+        //         }
+        //     }
+        // }
+        //stacks
+        stack<int> s;
+        for(int i=n2-1;i>=0;i--){
+            if(s.empty()){
+                s.push(nums2[i]);
+                d[nums2[i]]=-1;
+            }
+            else if(s.top()>nums2[i]){
+                d[nums2[i]]=s.top();
+                s.push(nums2[i]);
+            }
+            else{
+                while(!s.empty() && s.top()<=nums2[i]){
+                    s.pop();
+                }
+                if(s.empty()){
+                     s.push(nums2[i]);
+                     d[nums2[i]]=-1;
+                }
+                else{
+                    d[nums2[i]]=s.top();
+                    s.push(nums2[i]);
                 }
             }
+        }
+        for(int i=0;i<n1;i++){
+            ans[i]=d[nums1[i]];
         }
         return ans;
     }
