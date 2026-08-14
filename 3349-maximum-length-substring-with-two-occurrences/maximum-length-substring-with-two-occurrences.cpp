53885e96befb07acb543443419a2a1ba3a8ea54d
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-14 09:47:56 +0530

    Time: 2 ms (51.93%) | Memory: 9.7 MB (28.94%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -2,16 +2,31 @@ class Solution {
 public:
     int maximumLengthSubstring(string s) {
         //brute
-        int n=s.size(),i,j,ml=INT_MIN;
-        for(i=0;i<n;i++){
-            unordered_map<char,int> mpp;
-            for(j=i;j<n;j++){
-                mpp[s[j]]++;
-                if(mpp[s[j]]>2) break;
+        int n=s.size(),ml=INT_MIN;
+        // for(i=0;i<n;i++){
+        //     unordered_map<char,int> mpp;
+        //     for(j=i;j<n;j++){
+        //         mpp[s[j]]++;
+        //         if(mpp[s[j]]>2) break;
+        //     }
+        //     ml=max(ml,j-i);
+        //     if(j==n) break;
+        // }
+        //optimal-> sliding window
+        unordered_map<char,int> mpp;
+        int l=0,r=0;
+        while(r<n){
+            mpp[s[r]]++;
+            if(mpp[s[r]]>2){
+                ml=max(ml,r-l);
+                while(mpp[s[r]]>2){
+                    mpp[s[l]]--;
+                    l++;
+                }
             }
-            ml=max(ml,j-i);
-            if(j==n) break;
+            r++;
         }
+        ml=max(ml,r-l);
         return ml;
     }
 };
