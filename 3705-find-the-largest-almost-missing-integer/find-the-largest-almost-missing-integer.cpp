7374bfb13e011460b33a09296f97c21be6a4c5cc
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-18 19:40:36 +0530

    Time: 2 ms (57.89%) | Memory: 29.2 MB (65.79%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
-        //brute
+        //brute-O(n^3)
         int n=nums.size();
+        if(k==n) return *max_element(nums.begin(),nums.end());
         int res=-1;
-        for(int& num:nums){
-            int c=0;
-            int i=0,j=k-1;
-            while(j<n){
-                int subc=0;
-                for(int x=i;x<=j;x++){
-                    if(nums[x]==num) subc++;
-                }
-                if(subc) c++;
-                i++;
-                j++;
+        // for(int& num:nums){
+        //     int c=0;
+        //     int i=0,j=k-1;
+        //     while(j<n){
+        //         int subc=0;
+        //         for(int x=i;x<=j;x++){
+        //             if(nums[x]==num) subc++;
+        //         }
+        //         if(subc) c++;
+        //         i++;
+        //         j++;
+        //     }
+        //     if(c==1) res=max(res,num);
+        // }
+        //optimal-O(n)
+        unordered_map<int,int> mpp;
+        for(int& num:nums) mpp[num]++;
+
+        if(k==1){
+            for(int& num:nums){
+                if(mpp[num]==1) res=max(res,num);
             }
-            if(c==1) res=max(res,num);
+        }
+        else{
+            if(mpp[nums[0]]==1) res=max(res,nums[0]);
+            if(mpp[nums[n-1]]==1) res=max(res,nums[n-1]);
         }
         return res;
     }
