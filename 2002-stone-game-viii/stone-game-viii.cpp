0bea9d6ffddf351b237cff5be9a60704222b8ba2
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-24 18:06:27 +0530

    Time: 90 ms (95.17%) | Memory: 95.3 MB (49.63%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.cpp
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
+    // int n;
+    // int t[100001];
+
+    // int solve(int i, vector<int>& prefixSum) {
+    //     if(i == n-1) {
+    //         return prefixSum[n-1];
+    //     }
+
+    //     if(t[i] != -1)
+    //         return t[i];
+
+    //     int take = prefixSum[i] - solve(i+1, prefixSum);
+
+    //     int skip = solve(i+1, prefixSum);
+
+    //     return t[i] = max(take, skip);
+    // }
     int stoneGameVIII(vector<int>& stones) {
+        //recursion and memoization  - Topm down
+        // n = stones.size();
+        // memset(t, -1, sizeof(t));
+
+        // vector<int> prefixSum(n, 0);
+        // prefixSum[0] = stones[0];
+        
+
+        // for(int i = 1; i < n; i++) {
+        //     prefixSum[i] = prefixSum[i-1] + stones[i];
+        // }
+
+        // return solve(1, prefixSum); //Alice turns first
+        //bottom-up
         int n=stones.size();
         vector<int> prefix(n,0);
         prefix[0]=stones[0];
