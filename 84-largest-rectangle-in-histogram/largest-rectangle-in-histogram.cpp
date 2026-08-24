dfe3948bc68e9043b34e450bba57b9909f47503e
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-24 22:51:00 +0530

    Time: 39 ms (11.48%) | Memory: 83.3 MB (55.11%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -12,23 +12,28 @@ public:
         }
         return ans;
     }
-    vector<int> psel(vector<int>& nums){
-        vector<int> ans;
-        stack<pair<int,int>> st;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            while(!st.empty() && st.top().first>=nums[i]) st.pop();
-            if(st.empty()) ans.emplace_back(-1);
-            else ans.emplace_back(st.top().second);
-            st.push(make_pair(nums[i],i));
-        }
-        return ans;
-    }
+    // vector<int> psel(vector<int>& nums){
+    //     vector<int> ans;
+    //     stack<pair<int,int>> st;
+    //     int n=nums.size();
+    //     for(int i=0;i<n;i++){
+    //         while(!st.empty() && st.top().first>=nums[i]) st.pop();
+    //         if(st.empty()) ans.emplace_back(-1);
+    //         else ans.emplace_back(st.top().second);
+    //         st.push(make_pair(nums[i],i));
+    //     }
+    //     return ans;
+    // }
     int largestRectangleArea(vector<int>& heights) {
-        vector<int> l=psel(heights),r=nsel(heights);
-        int res=INT_MIN,n=heights.size();
+        vector<int> r=nsel(heights);
+        int res=INT_MIN,n=heights.size(),psel;
+        stack<pair<int,int>> st;
         for(int i=0;i<n;i++){
-            res=max(res,heights[i]*(r[i]-l[i]-1));
+            while(!st.empty() && st.top().first>=heights[i]) st.pop();
+            if(st.empty()) psel=-1;
+            else psel=st.top().second;
+            st.push(make_pair(heights[i],i));
+            res=max(res,heights[i]*(r[i]-psel-1));
         }
         return res;
     }
