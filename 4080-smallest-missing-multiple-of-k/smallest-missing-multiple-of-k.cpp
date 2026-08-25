19c8f66c3f161db89d6636e382b1093019a3c587
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-25 15:31:27 +0530

    Time: 3 ms (38.06%) | Memory: 25.1 MB (24.36%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
+        //brute
+        // int i=1;
+        // while(true){
+        //     int m=k*i;
+        //     bool yes=true;
+        //     for(int j=0;j<nums.size();j++){
+        //         if(nums[j]==m){
+        //             yes=false;
+        //             break;
+        //         }
+        //     }
+        //     if(yes) return m;
+        //     i++;
+        // }
+        // return 0;
+        //hashmap
+        unordered_set<int> mpp;
+        for(int& num:nums) mpp.insert(num);
         int i=1;
         while(true){
-            int m=k*i;
-            bool yes=true;
-            for(int j=0;j<nums.size();j++){
-                if(nums[j]==m){
-                    yes=false;
-                    break;
-                }
-            }
-            if(yes) return m;
+            if(mpp.find(k*i)==mpp.end()) return k*i;
             i++;
         }
         return 0;
