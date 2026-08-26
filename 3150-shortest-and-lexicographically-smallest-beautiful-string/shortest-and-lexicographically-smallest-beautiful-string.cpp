bf154e853c0376048432eba48e6f51d35b4bc842
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-08-26 12:44:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (64.74%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-        int n=s.size();
-        for(int len=k;len<=n;len++){
-            string res="";
-            for(int start=0;start<=n-len;start++){
-                string temp = s.substr(start,len);
-                int ones=0;
-                for(char& c:temp){
-                    if(c=='1') ones++;
+        //brute
+        // int n=s.size();
+        // for(int len=k;len<=n;len++){
+        //     string res="";
+        //     for(int start=0;start<=n-len;start++){
+        //         string temp = s.substr(start,len);
+        //         int ones=0;
+        //         for(char& c:temp){
+        //             if(c=='1') ones++;
+        //         }
+        //         if(ones==k){
+        //             if(res.empty() || temp<res) res=temp; 
+        //         }
+        //     }
+        //     if(!res.empty()) return res;
+        // }
+        // return "";
+        int n=s.size(),i=0,j=0,ones=0;
+        string res="";
+        while(j<n){
+            if(s[j]=='1') ones++;
+            while(ones>k || s[i]=='0'){
+                if(s[i]=='1'){
+                    ones--;
                 }
-                if(ones==k){
-                    if(res.empty() || temp<res) res=temp; 
-                }
+                i++;
             }
-            if(!res.empty()) return res;
+            if(ones==k){
+                int len=j-i+1;
+                string temp=s.substr(i,len);
+                if(res.empty() || len<res.size() || (temp.size()==res.size() && temp<res)) res=temp;
+            }
+            j++;
         }
-        return "";
+        return res;
     }
 };
